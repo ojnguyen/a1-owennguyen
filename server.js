777cfd95e98e3d8ebd065582dcbb37856fc20a24
Owen Nguyen <owennguyen18@gmail.com>
2026-08-28 11:00:31 -0400

    Built website

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
 const http = require('http'),
-      fs   = require('fs'),
-      port = 3000
+  fs = require('fs'),
+  port = 3000
 
-const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
+const server = http.createServer(function (request, response) {
+  switch (request.url) {
     case '/':
-      sendFile( response, 'index.html' )
+      sendFile(response, 'index.html')
       break
     case '/index.html':
-      sendFile( response, 'index.html' )
+      sendFile(response, 'index.html')
+      break
+    case '/styles.css':
+      sendFile(response, 'styles.css')
+      break
+    case '/color_palette.png':
+      sendFile(response, 'color_palette.png')
       break
     default:
-      response.end( '404 Error: File Not Found' )
+      response.end('404 Error: File Not Found')
   }
 })
 
-server.listen( process.env.PORT || port )
+server.listen(process.env.PORT || port)
 
-const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
-   })
+const sendFile = function (response, filename) {
+  // Added via Prof. Robert's/Kyle P's discord message to fix Render not rendering CSS
+  if (filename === 'styles.css') {
+    response.setHeader('Content-Type', 'text/css')
+  }
+
+  fs.readFile(filename, function (err, content) {
+    response.end(content, 'utf-8')
+  })
 }
